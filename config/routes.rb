5f8096479c1d6d6d7f7429dbadda245e37f93dd5
smaru1111 <marusou20@gmail.com>
2024-03-08 05:12:16 +0900

    mod cors設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  match '*path' => 'options_request#response_preflight_request', via: :options
   namespace 'api' do
     namespace 'v1' do
       resources :regexes
